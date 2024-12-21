678169ed447a5af09f26480c12f1a1c3830c06ee
innocentdavid <paulinnocent04@gmail.com>
2024-12-21 16:07:49 +0100

    c

---
+++ src/delete-App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function DeleteApp() {
+  return (
+    <div>DeleteApp</div>
+  )
+}
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import AboutUs from './pages/AboutUs';
+// import NewWelcomeScreen from './components/NewWelcomeScreen';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<NewWelcomeScreen />} />
+//         <Route path="/about" element={<AboutUs />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App; 
